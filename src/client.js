955e1c05d649393a38fd3da289f07818505733c2
Bethany Mattern <bethany.a.cable@gmail.com>
2022-07-19 16:05:04 -0400

    Created client & env for ID and token

---
+++ src/client.js
@@ -0,0 +1,14 @@
+import imageUrlBuilder from "@sanity/image-url";
+import sanityClient from "@sanity/client";
+
+export const client = sanityClient({
+  projectId: process.env.REACT_APP__SANITY_PROJECT_ID,
+  dataset: "production",
+  apiVersion: "2022-07-19",
+  useCdn: true,
+  token: process.env.REACT_APP__SANITY_TOKEN,
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
