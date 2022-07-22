3f72e0d4fa8e971857559e36432285d6ea1990a1
Bethany Mattern <bethany.a.cable@gmail.com>
2022-07-22 12:54:10 -0400

    Insert Experience into Skills Compononent & Styles. Fix NavDots

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -1,12 +1,14 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React from 'react'
 
 const NavigationDots = ({ active }) => {
   return (
     <div className='app__navigation'>
         {['home', 'about', 'work', 'skills', 'contact'].map((item, index) => (
-            <a 
-            href={`#${item}`}
-            key={item + index}
+            <a
+            href = {`#${item}`}
+            key = {item + index}
             className="app__navigation-dot"
             style={active === item ? {backgroungColor: '#313bac'} : {}}
             />
