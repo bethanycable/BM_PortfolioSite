a1150419ad62f811eeaaa956e4d0cba152855199
Bethany Mattern <bethany.a.cable@gmail.com>
2022-07-22 16:01:34 -0400

    Add styles to Testimonial Component & fix NavDots misspell

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -10,7 +10,7 @@ const NavigationDots = ({ active }) => {
             href = {`#${item}`}
             key = {item + index}
             className="app__navigation-dot"
-            style={active === item ? {backgroungColor: '#313bac'} : {}}
+            style={active === item ? {backgroundColor: '#313bac'} : {}}
             />
         ))}
     </div>
