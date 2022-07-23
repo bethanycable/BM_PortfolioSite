b4dc1c9b33d4d5ee7fe49203317ed15682ee54e5
Bethany Mattern <bethany.a.cable@gmail.com>
2022-07-23 00:02:46 -0400

    Add sanity config for netlify build

---
+++ src/dashboardConfig.js
@@ -0,0 +1,25 @@
+export default {
+  widgets: [
+    {
+      name: "netlify",
+      options: {
+        title: "My Netlify deploys",
+        sites: [
+          {
+            title: "Sanity Studio",
+            apiId: "e2b5b25e-c5fb-4aa9-be2f-0a095327a2f5",
+            buildHookId: "62db72770a983d487c1f4866",
+            name: "bethanymattern",
+          },
+          {
+            title: "Bethany Mattern",
+            apiId: "e2b5b25e-c5fb-4aa9-be2f-0a095327a2f5",
+            buildHookId: "62db72770a983d487c1f4866",
+            name: "bethanymattern",
+            url: "www.bethanycable.com",
+          },
+        ],
+      },
+    },
+  ],
+};
