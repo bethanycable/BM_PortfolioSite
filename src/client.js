a939537d3f7f9abfd0d2beba950a84dc43304981
Bethany Mattern <bethany.a.cable@gmail.com>
2022-07-23 11:59:44 -0400

    Fix error

--- src/client.js
+++ src/client.js
@@ -2,7 +2,7 @@ import imageUrlBuilder from "@sanity/image-url";
 import sanityClient from "@sanity/client";
 
 export const client = sanityClient({
-  projectId: process.env.REACT_APP__SANITY_PROJECT_ID,
+  projectId: process.env.REACT_APP__PROJECT_ID,
   dataset: "production",
   apiVersion: "2022-07-19",
   useCdn: true,
