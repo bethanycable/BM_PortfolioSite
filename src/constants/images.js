3256e29c6dc8e22466757af0acca338c821293e4
Bethany Mattern <bethany.a.cable@gmail.com>
2022-07-24 23:20:42 -0400

    Update Photography component with more pictures

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,9 +2,14 @@ import about01 from "../assets/about01.png";
 import about02 from "../assets/about02.png";
 import about03 from "../assets/about03.png";
 import about04 from "../assets/about04.png";
+import aniseSage from "../assets/anise-sage.jpg";
 import api from "../assets/api.png";
+import aquilegaCultorum from "../assets/aquilega-cultorum.jpg";
+import beachRose from "../assets/beach-rose.jpg";
+import beardedIris from "../assets/beach-rose.jpg";
 import bgIMG from "../assets/bgIMG.png";
 import bgWhite from "../assets/bgWhite.png";
+import bridge from "../assets/overgrown-bridge.jpg";
 import calibrachoa from "../assets/calibrachoa.jpg";
 import circle from "../assets/circle.svg";
 import cpp from "../assets/cpp.png";
@@ -18,21 +23,34 @@ import git from "../assets/git.png";
 import graphql from "../assets/graphql.png";
 import html from "../assets/html.png";
 import hypericum from "../assets/hypericum.jpg";
+import indianLantana from "../assets/west-indian-lantana.jpg";
 import javascript from "../assets/javascript.png";
+import lambsEar from "../assets/lambs-ear.jpg";
 import lantana from "../assets/lantana.jpg";
+import lilacTree from "../assets/lilac-tree.jpg";
 import logo from "../assets/BM-logo-noback.png";
+import marvelPeru from "../assets/marvel-of-peru.jpg";
 import mobile from "../assets/mobile.png";
 import mu5 from "../assets/mu5.png";
 import node from "../assets/node.png";
+import orangeDayLily from "../assets/orange-day-lily.jpg";
+import peanutCactus from "../assets/peanut-cactus.jpg";
 import pinkRose from "../assets/pink_rose.jpg";
+import pinkRose2 from "../assets/pink-rose2.jpg";
+import ponderosaLemon from "../assets/ponderosa-lemon.jpg";
 import profile from "../assets/profile.png";
 import python from "../assets/python.png";
 import react from "../assets/react.png";
+import redRose from "../assets/red-rose.jpg";
 import redux from "../assets/redux.png";
+import sacredDatura from "../assets/sacred-datura.jpg";
 import sass from "../assets/sass.png";
 import stBrunoLily from "../assets/st-bruno-lily.jpeg";
+import telephoneBooth from "../assets/telephone-booth.jpg";
+import tigerLily from "../assets/tiger-lily.jpg";
 import typescript from "../assets/typescript.png";
 import vue from "../assets/vue.png";
+import yellowDayLily from "../assets/yellow-daylily.jpg";
 
 const images = {
   api,
@@ -65,11 +83,29 @@ const images = {
   logo,
   calibrachoa,
   crapeMyrtle,
+  crepeMyrtle,
   hypericum,
   lantana,
   pinkRose,
-  crepeMyrtle,
   stBrunoLily,
+  aniseSage,
+  aquilegaCultorum,
+  beachRose,
+  beardedIris,
+  lambsEar,
+  lilacTree,
+  marvelPeru,
+  orangeDayLily,
+  bridge,
+  peanutCactus,
+  pinkRose2,
+  ponderosaLemon,
+  redRose,
+  sacredDatura,
+  telephoneBooth,
+  tigerLily,
+  indianLantana,
+  yellowDayLily,
 };
 
 export default images;
