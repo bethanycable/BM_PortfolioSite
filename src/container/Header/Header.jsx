ab8f2c7e3f3f70999fe985e203ffb93b22d18f49
Bethany Mattern <bethany.a.cable@gmail.com>
2023-06-05 23:34:27 -0400

    Adjust the header component and styling and update works categories

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,57 +1,48 @@
-import './Header.scss'
+import './Header.scss';
 
-import {AppWrap} from '../../wrapper'
-import React from 'react'
-import { images } from '../../constants'
-import { motion } from 'framer-motion'
+import { AppWrap } from '../../wrapper';
+import React from 'react';
+import { images } from '../../constants';
+import { motion } from 'framer-motion';
 
 const scaleVariants = {
   whileInView: {
     scale: [0, 1],
-    opacity: [0,1],
+    opacity: [0, 1],
     transition: {
       duration: 1,
-      ease: 'easeInOut'
-    }
-  }
-}
+      ease: 'easeInOut',
+    },
+  },
+};
 
 const Header = () => {
   return (
-    <div className='app__header app__flex'>
+    <div className="app__header app__flex">
       <motion.dev
-        whileInView={{ x: [-100,0], opacity: [0,1]}}
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
         className="app__header-info"
       >
-        <div className='app__header-badge'>
-          <div className='badge-cmp app__flex'>
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
             <span>👋</span>
-            <div style={{marginLeft: 20}}>
-              <p className='p-text'>Hello, I am</p>
-              <h1 className='head-text'>Bethany</h1>
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text">Hello, I am</p>
+              <h1 className="head-text">Bethany</h1>
             </div>
-          </div>  {/* badge-cmp*/}
-          <div className='tag-cmp app__flex'>
-          <p className='p-text'>Web Developer</p>
-          <p className='p-text'>Book Nerd</p>
+          </div>{' '}
+          {/* badge-cmp*/}
+          <div className="tag-cmp app__flex">
+            <p className="p-text">Web Developer</p>
+            <p className="p-text">Book Nerd</p>
           </div>
-
         </div>
-        {/* <motion.div
-          whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-          duration={{ duration: 0.5 }}
-          className='app__header-btn'
-        >
-          <a href="Contact.jsx">Contact Me!</a>
-        </motion.div>  */}
       </motion.dev>
 
-
-      
       <motion.div
-        whileInView={{opacity: [0, 1] }}
-        transition={{duration: 0.5, delayChildren: 0.5 }}
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
         className="app__header-img"
       >
         <img src={images.profile} alt="profile_bg" />
@@ -63,13 +54,13 @@ const Header = () => {
         className="app__header-circles"
       >
         {[images.sass, images.javascript, images.react].map((circle, index) => (
-          <div className='circle-cmp app__flex' key={`circle=${index}`}>
+          <div className="circle-cmp app__flex" key={`circle=${index}`}>
             <img src={circle} alt="circle" />
           </div>
         ))}
       </motion.div>
     </div>
-  )
-}
+  );
+};
 
-export default AppWrap(Header, 'home')
+export default AppWrap(Header, 'home');
--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -1,95 +1,101 @@
-import './Work.scss'
+import './Work.scss';
 
-import {AiFillEye, AiFillGithub} from 'react-icons/ai'
-import { AppWrap, MotionWrap } from '../../wrapper'
-import React, { useEffect, useState } from 'react'
-import { client, urlFor } from '../../client'
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { AppWrap, MotionWrap } from '../../wrapper';
+import React, { useEffect, useState } from 'react';
+import { client, urlFor } from '../../client';
 
-import { motion } from 'framer-motion'
+import { motion } from 'framer-motion';
 
 const Work = () => {
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
 
-  const [activeFilter, setActiveFilter] = useState('All')
-  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1})
-
-  const [works, setWorks] = useState([])
-  const [filterWork, setFilterWork] = useState([])
+  const [works, setWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState([]);
 
   useEffect(() => {
-    const query = '*[_type == "works"]'
+    const query = '*[_type == "works"]';
 
-    client.fetch(query)
-      .then((data) => {
-        setWorks(data)
-        setFilterWork(data)
-      })
-
-  }, [])
+    client.fetch(query).then((data) => {
+      setWorks(data);
+      setFilterWork(data);
+    });
+  }, []);
 
   const handleWorkFilter = (item) => {
-    setActiveFilter(item)
-    setAnimateCard([{ y: 100, opacity: 0}])
+    setActiveFilter(item);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
 
     setTimeout(() => {
-      setAnimateCard([{ y: 0, opacity: 1}])
+      setAnimateCard([{ y: 0, opacity: 1 }]);
 
-      if(item === 'All') {
-        setFilterWork(works)
+      if (item === 'All') {
+        setFilterWork(works);
       } else {
-        setFilterWork(works.filter((work) => work.tags.includes(item)))
+        setFilterWork(works.filter((work) => work.tags.includes(item)));
       }
-
-    }, 500)
-  }
+    }, 500);
+  };
 
   return (
     <>
-      <h2 className='head-text'>My Creative <span>Portfolio</span> Section</h2>
+      <h2 className="head-text">
+        My Creative <span>Portfolio</span> Section
+      </h2>
 
-      <div className='app__work-filter'>
-        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map(( item, index ) => (
-          <div
-            key={index}
-            onClick={() => handleWorkFilter(item)}
-            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
-          >
-            {item}
-          </div>
-        ))}
+      <div className="app__work-filter">
+        {['UI/UX', 'Web App', 'Reactjs', 'TypeScript', 'All'].map(
+          (item, index) => (
+            <div
+              key={index}
+              onClick={() => handleWorkFilter(item)}
+              className={`app__work-filter-item app__flex p-text ${
+                activeFilter === item ? 'item-active' : ''
+              }`}
+            >
+              {item}
+            </div>
+          )
+        )}
       </div>
 
       <motion.div
-        animate = { animateCard }
-        transition = {{ duration: 0.5, delayChildren: 0.5 }}
-        className = 'app__work-porfolio'
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__work-porfolio"
       >
         {filterWork.map((work, index) => (
-          <div className = 'app__work-item app__flex' key = {index}>
-            <div className = 'app__work-img app__flex'>
-              <img src = {urlFor(work.imgUrl)} alt = {work.name} />
+          <div className="app__work-item app__flex" key={index}>
+            <div className="app__work-img app__flex">
+              <img src={urlFor(work.imgUrl)} alt={work.name} />
 
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
-                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
-                className= 'app__work-hover app__flex'
+                transition={{
+                  duration: 0.25,
+                  ease: 'easeInOut',
+                  staggerChildren: 0.5,
+                }}
+                className="app__work-hover app__flex"
               >
-                <a href={work.projectLink} target='_blank' rel='noreferrer'>
+                <a href={work.projectLink} target="_blank" rel="noreferrer">
                   <motion.div
-                    whileInView={{scale: [0, 1]}}
+                    whileInView={{ scale: [0, 1] }}
                     whileHover={{ opacity: [1, 0.9] }}
-                    transition={{ duration: 0.25 }} 
-                    className='app__flex'
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
                   >
                     <AiFillEye />
                   </motion.div>
                 </a>
 
-                <a href={work.codeLink} target='_blank' rel='noreferrer'>
+                <a href={work.codeLink} target="_blank" rel="noreferrer">
                   <motion.div
-                    whileInView={{scale: [0, 1]}}
+                    whileInView={{ scale: [0, 1] }}
                     whileHover={{ opacity: [1, 0.9] }}
-                    transition={{ duration: 0.25 }} 
-                    className='app__flex'
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
                   >
                     <AiFillGithub />
                   </motion.div>
@@ -97,23 +103,25 @@ const Work = () => {
               </motion.div>
             </div>
 
-            <div className='app__work-content app__flex'>
-              <h4 className='bold-text'>{work.title}</h4>
-              <p className='p-text' style={{marginTop: 10}}>{work.description}</p>
+            <div className="app__work-content app__flex">
+              <h4 className="bold-text">{work.title}</h4>
+              <p className="p-text" style={{ marginTop: 10 }}>
+                {work.description}
+              </p>
 
-              <div className='app__work-tag app__flex'>
-                <p className='p-text'>{work.tags[0]}</p>
+              <div className="app__work-tag app__flex">
+                <p className="p-text">{work.tags[0]}</p>
               </div>
             </div>
           </div>
         ))}
       </motion.div>
     </>
-  )
-}
+  );
+};
 
 export default AppWrap(
-  MotionWrap(Work, 'app__works'), 
-  'work', 
+  MotionWrap(Work, 'app__works'),
+  'work',
   'app__primarybg'
-)
+);
